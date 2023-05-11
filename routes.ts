c362fae8979d009d94963bca1db6e177249313eb
Murendeni Mukwevho <murendenim@girlcode.co.za>
2023-05-11 16:36:47 +0200

    Fixed update route

--- routes.ts
+++ routes.ts
@@ -4,7 +4,7 @@ import {getPrices, getScrap, index} from "./controllers.ts";
 const router = new Router()
 
 router.get('/', index)
-router.get('/prices/', getPrices)
+router.get('/prices', getPrices)
 router.get('/scrap', getScrap)
 
 export default router;
