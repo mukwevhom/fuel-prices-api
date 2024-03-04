bb6617098c325d535451ae1dfa74498e4d959701
Murendeni Mukwevho <murendenim@girlcode.co.za>
2024-03-04 23:15:36 +0200

    Minor SQL change

--- utils/statements.sql
+++ utils/statements.sql
@@ -1,4 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS hstore;
 
 CREATE TABLE IF NOT EXISTS coastal_prices (
 	id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -22,3 +23,13 @@ CREATE TABLE IF NOT EXISTS inland_prices (
 	illuminating_paraffin DOUBLE PRECISION DEFAULT NULL,
 	liquefied_petroleum_gas DOUBLE PRECISION DEFAULT NULL
 );
+
+CREATE TABLE coastal_prices_update (
+  id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+  month TEXT NOT NULL UNIQUE,
+  prices hstore,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE coastal_prices_update
+ADD CONSTRAINT check_prices_types CHECK (prices ?& 'float_key'::text[] = ANY(regexp_split_to_array('[+-]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?', prices ?& 'float_key'::text[])));
