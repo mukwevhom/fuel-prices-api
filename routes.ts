7783628f3a3ed5112751aa9e5154d12dc10ea080
Murendeni Mukwevho <mukwevhom@live.com>
2025-05-05 22:17:25 +0200

    feat: add historical scraping functionality and update fuel price extraction logic

--- routes.ts
+++ routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "@oak/oak/router";
-import {getPrices, getScrap, index} from "./controllers.ts";
+import {getPrices, getScrap, getScrapPast, index} from "./controllers.ts";
 
 const router = new Router()
 
 router.get('/', index)
 router.get('/prices', getPrices)
 router.get('/scrap', getScrap)
+router.get('/scrap-past', getScrapPast)
 
 export default router;
--- utils/scraper.ts
+++ utils/scraper.ts
@@ -21,9 +21,47 @@ const scraper = async () => {
         }
 
         fuelPrices.coastal = getPrices(coastalTable, 1, 6)
-        fuelPrices.inland = getPrices(inlandTable, 1, 6)
+        fuelPrices.inland = getPrices(inlandTable, 1, 7)
     
         return fuelPrices
+    } catch(error) {
+        throw error
+    }
+}
+
+const historicalScraper = async () => {
+    try {
+        const res = await fetch(url);
+        const html = await res.text();
+        const $ = cheerio.load(html)
+
+        const pastFuelPrices: FuelPrice[] = []
+        const pastFuelPricesItems = $('.tf-accordion .tf-accordion-item')
+
+        for (const item of pastFuelPricesItems) {
+            const el = $(item)
+            const elTitle = el.find('.accordion-title')
+            const elContent = el.find('.accordion-content')
+
+            const year = elTitle.text().trim()
+
+            if (year && elContent.length) {
+                const fuelPrices: FuelPrice = {
+                    "year": Number(year),
+                    "coastal": [],
+                    "inland": []
+                }
+
+                const fuelPriceTable = elContent.find(`#tablepress-${year}`)
+
+                fuelPrices.coastal = getPrices(fuelPriceTable, 2, 7)
+                fuelPrices.inland = Number(year) === 2023 ? getPrices(fuelPriceTable, 10, 16) : getPrices(fuelPriceTable, 9, 15)
+
+                pastFuelPrices.push(fuelPrices)
+            }
+        }
+    
+        return pastFuelPrices
     } catch(error) {
         throw error
     }
@@ -36,7 +74,7 @@ const getPrices = (fuelPriceTable: cheerio.Cheerio<cheerio.Element>, start: numb
     for (let i = start; i <= end; i++) {
         const currRow = $(`tbody tr:nth-child(${i})`)
 
-        const fuelName = currRow.find('td:first-child').text().trim()
+        const fuelName = currRow.find('td:first-child').text().trim().replace(/\s*\*+$/gmi, "")
         const elFuelPrices = currRow.find('td:not(:first-child)')
 
         const tmpFuelInfo:fuelInfo = {
@@ -53,7 +91,6 @@ const getPrices = (fuelPriceTable: cheerio.Cheerio<cheerio.Element>, start: numb
     }
 
     return tempInfoArr
-
 }
 
 const getYear = (fuelPriceTable: cheerio.Cheerio<cheerio.Element>) => {
@@ -65,4 +102,4 @@ const getYear = (fuelPriceTable: cheerio.Cheerio<cheerio.Element>) => {
     return Number('20'+strYear)
 }
 
-export default scraper
+export { scraper, historicalScraper }
